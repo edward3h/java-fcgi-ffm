0d312b556b0726e5c3b45959cf857f408356cf0c
Edward Harman <jaq+github@ethelred.org>
2026-06-18 09:39:50 -0400

    Add test-avaje App entry point

---
+++ test-avaje/src/main/java/red/ethel/fcgi/testavaje/App.java
@@ -0,0 +1,10 @@
+/* (C) Edward Harman 2026 */
+package red.ethel.fcgi.testavaje;
+
+import io.avaje.jex.AvajeJex;
+
+public class App {
+    static void main() {
+        AvajeJex.start();
+    }
+}
